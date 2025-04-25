d8646a4928c8a4b374a241384543753d4133b76b
aishwaryaPangale <aishwaryapangale0@gmail.com>
2025-04-25 14:09:59 +0530

    changes

--- OnlineExamApp/src/main/java/com/example/demo/Controller/RegisterController.java
+++ OnlineExamApp/src/main/java/com/example/demo/Controller/RegisterController.java
@@ -1,12 +1,21 @@
 package com.example.demo.Controller;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.example.demo.Model.Register;
@@ -103,7 +112,20 @@ public class RegisterController {
         int count = regService.getStudentCount();
         return Map.of("count", count);
     }
-    
+//    attended test
+    @GetMapping("/attended/count")
+    public Map<String, Integer> getAttendedStudentCount() {
+        Map<String, Integer> response = new HashMap<>();
+        response.put("count", regService.getAttendedStudentCount());
+        return response;
+    }
+//not Attended Test
+    @GetMapping("/not-attended/count")
+    public Map<String, Integer> getNotAttendedStudentCount() {
+        Map<String, Integer> response = new HashMap<>();
+        response.put("count", regService.getNotAttendedStudentCount());
+        return response;
+    }
     //test Score
 //    @GetMapping("/students/{studentId}/tests")
 //    public List<Register> getStudentTests(@PathVariable int studentId) {
